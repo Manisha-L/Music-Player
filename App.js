f6ed1d9076bc4076d12983af2cdf90d5477717e7
Manisha-L <manisha.lakshmipathula3@gmail.com>
2021-10-14 18:10:27 +0100

    Music Player app

--- App.js
+++ App.js
@@ -1,21 +1,265 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useRef, useEffect, useState } from "react";
+import { Audio, Video } from "expo-av";
 
-export default function App() {
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Animated,
+} from "react-native";
+
+import songs from "./data";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+const { width } = Dimensions.get("window");
+
+const clonedPlaybackInstance = new Audio.Sound();
+const App = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const songSlider = useRef(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playbackInstance, setPlaybackInstance] = useState(null);
+  const [volume, setVolume] = useState(1.0);
+  const [isBuffering, setIsBuffering] = useState(true);
+  // console.log("printttt", songs[currentIndex].url);
+  const onPlaybackStatusUpdate = (status) => {
+    setIsBuffering(status.isBuffering);
+  };
+  const loadAudio = async () => {
+    try {
+      const src = songs[currentIndex].url;
+
+      const status = {
+        shouldPlay: isPlaying,
+        volume: volume,
+      };
+
+      clonedPlaybackInstance.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+      await clonedPlaybackInstance.loadAsync(src, status);
+      await clonedPlaybackInstance.playAsync();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      try {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: false,
+          interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+          playsInSilentModeIOS: true,
+          interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+          shouldDuckAndroid: true,
+          staysActiveInBackground: true,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    scrollX.addListener(({ value }) => {
+      const index = Math.round(value / width);
+      setCurrentIndex(index);
+    });
+    return () => {
+      scrollX.removeAllListeners();
+    };
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const s = await clonedPlaybackInstance.getStatusAsync();
+      if (s.isLoaded) {
+        await clonedPlaybackInstance.unloadAsync();
+        await loadAudio();
+        setIsPlaying(true);
+      }
+    })();
+  }, [currentIndex]);
+
+  const handlePlayPause = async () => {
+    const s = await clonedPlaybackInstance.getStatusAsync();
+
+    if (s.isPlaying) {
+      await clonedPlaybackInstance.pauseAsync();
+      setIsPlaying(false);
+      return;
+    } else if (s.isLoaded) {
+      await clonedPlaybackInstance.playAsync();
+      setIsPlaying(true);
+      return;
+    }
+    await loadAudio();
+    setIsPlaying(true);
+  };
+
+  const handlePreviousTrack = () => {
+    setCurrentIndex(currentIndex - 1);
+    songSlider.current.scrollToOffset({
+      offset: (currentIndex - 1) * width,
+    });
+    setCurrentIndex(currentIndex);
+  };
+
+  const handleNextTrack = () => {
+    setCurrentIndex(currentIndex + 1);
+    songSlider.current.scrollToOffset({
+      offset: (currentIndex + 1) * width,
+    });
+    setCurrentIndex(currentIndex);
+  };
+
+  const renderSongs = ({ item }) => {
+    return (
+      <Animated.View style={styles.imgWrapper}>
+        <Image source={item.image} style={styles.img}></Image>
+      </Animated.View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <StatusBar style="auto" />
+        <View style={styles.musicWrapper}>
+          <FontAwesome name="music" size={35} color="deeppink" />
+          <Text style={styles.music}>Music</Text>
+        </View>
+
+        <Animated.FlatList
+          ref={songSlider}
+          data={songs}
+          renderItem={renderSongs}
+          keyExtractor={(item) => item.id}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: true }
+          )}
+        />
+
+        <View style={styles.bottomContainer}>
+          <View style={styles.details}>
+            <Text
+              style={{ color: "deeppink", fontSize: 25, fontWeight: "bold" }}
+            >
+              {songs[currentIndex].title}
+            </Text>
+            <Text style={{ color: "orangered" }}>
+              {songs[currentIndex].artist}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: 8,
+            }}
+          >
+            <View style={styles.musicControls}>
+              <TouchableOpacity onPress={() => handlePreviousTrack()}>
+                <Ionicons
+                  name="md-play-skip-back"
+                  size={55}
+                  color="darkorange"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => {
+                  handlePlayPause();
+                }}
+              >
+                {isPlaying ? (
+                  <Ionicons
+                    name="md-pause-circle"
+                    size={75}
+                    color="darkorange"
+                  />
+                ) : (
+                  <Ionicons
+                    name="md-play-circle"
+                    size={75}
+                    color="darkorange"
+                  />
+                )}
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  handleNextTrack();
+                }}
+              >
+                <Ionicons
+                  name="md-play-skip-forward"
+                  size={55}
+                  color="darkorange"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  musicWrapper: {
+    justifyContent: "center",
+    marginTop: 80,
+    flexDirection: "row",
+  },
+  music: {
+    color: "orangered",
+    fontSize: 35,
+    marginHorizontal: 5,
+  },
+  imgWrapper: {
+    marginTop: 20,
+    width: width,
+    height: 340,
+  },
+
+  img: {
+    borderRadius: 20,
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  bottomContainer: {
+    marginBottom: 50,
+    height: 200,
+  },
+  details: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+
+  musicControls: {
+    width: 300,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
+export default App;
---
+++ data.js
@@ -0,0 +1,40 @@
+const songs = [
+  {
+    title: "Not Afraid",
+    artist: "Eminem",
+    image: require("./assets/images/notAfraid.jpg"),
+    url: require("./assets/songs/notAfraid.mp3"),
+    id: "1",
+  },
+  {
+    title: "Who Says",
+    artist: "Selena Gomez",
+    image: require("./assets/images/who.jpg"),
+    url: require("./assets/songs/who.mp3"),
+    id: "2",
+  },
+  {
+    title: "Shape of You",
+    artist: "Ed Sheeran",
+    image: require("./assets/images/shape.jpg"),
+    url: require("./assets/songs/shape.mp3"),
+    id: "3",
+  },
+
+  {
+    title: "Dance Monkey",
+    artist: "Tones and I",
+    image: require("./assets/images/dance.jpg"),
+    url: require("./assets/songs/dance.mp3"),
+    id: "4",
+  },
+
+  {
+    title: "Chandelier",
+    artist: "Sia Furler",
+    image: require("./assets/images/chandelier.jpg"),
+    url: require("./assets/songs/chandelier.mp3"),
+    id: "5",
+  },
+];
+export default songs;
